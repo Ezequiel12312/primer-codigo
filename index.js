2927a5efe02fde0493e1bac0378b11900a6db342
ezequiel <2019141044@unh.edu.pe>
2021-11-15 21:30:32 -0500

    en javascript

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const btn = document.getElementById("add");
 btn.addEventListener("click" , ()=>{
 
     console.log("esto esta siendo bueno");
+    console.log("esto es para todo los que estan viendo en vivo y en directo xd");
 
 });
